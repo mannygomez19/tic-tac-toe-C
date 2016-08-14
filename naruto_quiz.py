bb2f6564170be2477ac4b72fc3407c6ce501bea7
Manuel Takeshi Gomez <mg658221@gmail.com>
2016-08-14 10:05:00 -0400

    Update naruto_quiz.py

--- naruto_quiz.py
+++ naruto_quiz.py
@@ -1,3 +1,4 @@
+from fractions import Fraction
 
 print("\nHere is a quiz to determine which Naruto character you are")
 
@@ -16,8 +17,6 @@ for letter, answer in sorted(dictionary1.items()):
 
 response1 = raw_input("answer (letter only): ")
 "-----------------------------------------------------------"
-# Find a way to accept the answer and then use all of the other answers together
-# to make a personality portfolio on some1.
 
 print("\n2) How would you approach an opponent in a fight?")
 
@@ -34,7 +33,7 @@ for letter, answer in sorted(dictionary2.items()):
 
 response2 = raw_input("answer (letter only): ")
 "---------------------------------------------------------------"
-print("\n3) What animal do you identify with most?") # work on.
+print("\n3) What animal do you identify with most?") 
 
 dictionary3 = {
         '\ta': ' Rabbit',
@@ -85,7 +84,31 @@ response5 = raw_input("answer (letter only): ")
 
 "-------------------------------------------------------------------"
 
- 
+# Find a way to accept the answer and then use all of the other answers together
+# to make a personality portfolio on some1.
+
+sakura_answer_list = {'a': response1, 'e': response2, 'a': response3,
+                      'd': response4, 'b': response5}
+shikamaru_answer_list = ['d', 'a', 'b', 'b', 'e']
+kakashi_answer_list = {'e': response1, 'd': response2, 'd': response3,
+                       'e': response4, 'c': response5}
+naruto_answer_list = ['b', 'b', 'c', 'a', 'd']
+sasuke_answer_list = ['c', 'c', 'e', 'c', 'a']
+
+for key, value in sakura_answer_list.items():
+    if sakura_answer_list >= Fraction(3, 5):
+        print("Your are Sakura!") # find a way to somehow call the answers from response 1 -5 
+    else:
+        break
+
+for key, value in kakashi_answer_list.items():
+    if kakashi_answer_list >= Fraction(3, 5):
+        print("You are Kakashi Sensei!")
+        
+
+
+
+
 
 
 
