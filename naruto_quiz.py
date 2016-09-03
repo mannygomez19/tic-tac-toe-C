9ef5e346f287abc4e9802efbbd4c1ddc018c5605
Manuel Takeshi Gomez <mg658221@gmail.com>
2016-09-02 19:25:04 -0500

    Add files via upload

--- naruto_quiz.py
+++ naruto_quiz.py
@@ -1,5 +1,6 @@
 print("\nHere is a quiz to determine which Naruto character you are")
 
+    
 sakura_points = 0
 sakura_points = int(sakura_points)
 
@@ -15,7 +16,7 @@ shikamaru_points = int(shikamaru_points)
 sasuke_points = 0
 sasuke_points = int(sasuke_points)
 
-
+"--------------------------------------------------------------"
 def qstn1():
     
     print \
@@ -26,7 +27,7 @@ def qstn1():
               4) Confront who/what is angering you
               5) Analyze it and think it out """
 
-    q1 = input("\n Answer: ")
+    q1 = raw_input("\n Answer: ")
 
     if q1 == "1":
         global sakura_points
@@ -59,7 +60,7 @@ def qstn2():
               4)Allow them to exhaust themselves
               5)Work with a partner to defeat the adversary """
         
-    q2 = input("\n Answer: ")
+    q2 = raw_input("\n Answer: ")
 
     if q2 == "1":
         global shikamaru_points
@@ -92,7 +93,7 @@ def qstn3():
               3)Tiger
               4)Hawk
               5)Snake """
-    q3 = input("\n Answer: ")
+    q3 = raw_input("\n Answer: ")
 
     if q3 == "1":
         global sakura_points
@@ -124,7 +125,7 @@ def qstn4():
               3)Fire
               4)Water
               5)Lightening"""
-    q4 = input("\n Answer: ")
+    q4 = raw_input("\n Answer: ")
 
     if q4 == "1":
         global naruto_points
@@ -158,7 +159,7 @@ def qstn5():
               3)Independent and blunt
               4)Outgoing and energetic
               5)Smart and lazy"""
-    q5 = input("\n Answer: ")
+    q5 = raw_input("\n Answer: ")
 
     
     if q5 == "1":
@@ -183,9 +184,13 @@ def qstn5():
 qstn5()
 
 "-------------------------------------------------------------------"
+
+while character_points():
+    print("The results are...")
+
 if sakura_points >= 3:
     print("You are Sakura!")
-    
+
 if sasuke_points >= 3:
     print("You are Sasuke!")
     
@@ -197,13 +202,52 @@ if naruto_points >= 3:
     
 if shikamaru_points >= 3:
     print("You are Shikamaru!")
+
+elif character_points() < 3:
+    def qstn6():
+        print \
+            """6) Do you (or did you) like going to school?
+                  1)Fuck no. I am too energetic to sit that long!
+                  2)It's okay, but I like learning practical things
+                  3)Yes! I love learning
+                  4)It's okay, but find it hard to interact with so many people
+                  5)No, I prefer to skip class"""
+        q6 = raw_input("\n Answer: ")
+
     
+        if q6 == "1":
+            global naruto_points
+            naruto_points += 2
+        elif q6 == "2":
+            global kakashi_points
+            kakashi_points += 2
+        elif q6 == "3":
+            global sakura_points
+            sakura_points += 2
+        elif q6 == "4":
+            global sasuke_points
+            sasuke_points += 2
+        elif q6 == "5":
+            global shikamaru_points
+            shikamaru_points += 2
+        else:
+            print("\n Please choose answer choices 1-5")
+            qstn6()
 
+    qstn6()
 
+    if sakura_points >= 4:
+        print("You are Sakura!")
+    if naruto_points >= 4:
+        print("You are Naruto!")
+    if kakashi_points >= 4:
+        print("You are Kakashi!")
+    if shikamaru_points >= 4:
+        print("You are Shikamaru!")
+    if sasuke_points >= 4:
+        print("You are Sasuke!")
 
-
-
-
+character_points()       
 
 
 
